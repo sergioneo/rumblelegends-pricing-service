4f1aeeeec1ab11ac06dbefc01c1a89926b40163a
Sergio Yanez <sergioy@amazon.com>
2018-08-27 05:51:59 -0700

    USD and ETH prices

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "typhoeus"
 
 gem 'rack-cors', :require => 'rack/cors'
 
+gem 'cryptocompare'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -9,9 +9,20 @@ class PricingController < ApplicationController
 
   def beast
 
+    require 'cryptocompare'
+
   	req_url = ENV["CONTRACT_MASTER_SERVICE"]+"beast/get_price/"+params[:id]
   	price_structure =  Typhoeus.get(req_url, followlocation: true).body
 
-  	render json: price_structure
+    usd_conversion = Cryptocompare::Price.find('ETH', 'USD')
+
+    eth_value = price_structure["price"].to_f
+    usd_value = eth_value * usd_conversion["ETH"]["USD"]
+
+    return_structure = Hash.new
+    return_structure["eth"] = eth_value
+    return_structure["usd"] = usd_value
+
+  	render json: return_structure
   end
 end
